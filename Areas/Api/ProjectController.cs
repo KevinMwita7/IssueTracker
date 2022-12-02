817a87ee6398834085a54b1cbfd5df36dbbfc7bc
KevinMwita7 <kevinmwita7@gmail.com>
2022-12-02 22:52:08 +0000

    delete swimlane

--- Areas/Api/ProjectController.cs
+++ Areas/Api/ProjectController.cs
@@ -1,6 +1,7 @@
 using IssueTracker.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 
 namespace IssueTracker.Areas.Api {    
@@ -13,6 +14,7 @@ namespace IssueTracker.Areas.Api {
             _context = context;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Details(Guid id) {
             if (id == null || _context.Projects == null) return NotFound();
 
@@ -24,6 +26,7 @@ namespace IssueTracker.Areas.Api {
             });
         }
 
+        [HttpGet]
         public async Task<IActionResult> Members(Guid id) {
             if (id == null || _context.Projects == null) return NotFound();
 
@@ -36,6 +39,7 @@ namespace IssueTracker.Areas.Api {
             });
         }
 
+        [HttpGet]
         public async Task<IActionResult> Swimlanes(Guid id) {
             if (id == null || _context.Projects == null) return NotFound();
             
@@ -46,6 +50,24 @@ namespace IssueTracker.Areas.Api {
             return ViewComponent("ProjectSwimlanes", new {
                 project = Project
             });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Swimlane(Guid projectId, Guid swimlaneId) {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(projectId);
+            Console.WriteLine(swimlaneId);
+            Console.WriteLine("---------------------------");
+            if (projectId == null || swimlaneId == null || _context.Swimlane == null) return NotFound();
+
+            var Swimlane = _context.Swimlane.Where(s => s.Id == swimlaneId).Where(s => s.ProjectId == projectId).First();
+
+            if (Swimlane != null) {
+                _context.Swimlane.Remove(Swimlane);
+                await _context.SaveChangesAsync();
+            }
+
+            return Ok();
         }
 
         public IActionResult AddMember() {
